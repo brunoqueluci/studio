6d5b8c06cd5b8a0dbbcd96d6ca51f1c243b32a9e
Bruno Queluci <brunoqueluci@gmail.com>
2022-10-19 15:32:05 -0300

    Página em BootStrap para informar que há erro de login em suas credenciais

---
+++ Errologin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="css\style.css">
+    <title>Studio | Sistema de Gereciamento</title>
+</head>
+<body>
+    <div class="alert alert-danger" role="alert">
+        <h4 class="alert-heading">Erro ao entar no sistema!</h4>
+        <p>Possivelmente seu usuario não esta cadatrado ou dados de acesso inválidos</p>
+        <hr>
+        <p>Verifique com adminstrador do sistema sobre esta mensagem.</p>
+        <p>Dados Erro: e-mail ou senha inválidos.</p>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
