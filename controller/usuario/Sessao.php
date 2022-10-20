0a7fe2563e5887e7edca4020c2484c47d3d6f433
Bruno Queluci <brunoqueluci@gmail.com>
2022-10-20 18:05:14 -0300

    Pequena alteração da função.

--- controller/usuario/Sessao.php
+++ controller/usuario/Sessao.php
@@ -1,1 +1,22 @@
 <?php
+class Sessao
+{
+    function dadosSessao($dados_banco)
+    {
+        //id, nome, email, senha, data_cadastro, status (Campos do banco);
+        
+        $_SESSION['id'] = $dados_banco["id"];
+        $_SESSION['nome'] = $dados_banco["nome"];
+        $_SESSION['status'] = $dados_banco["status"];
+       
+        return $_SESSION;
+    }
+    
+    function abreSessao()
+    {
+        session_start();
+        //session_unset();
+       // session_destroy();
+        
+    }
+}
