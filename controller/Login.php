16a8d041ccc48ac3e03f46377f0cba8c482b62cf
Bruno Queluci <brunoqueluci@gmail.com>
2022-10-20 09:51:27 -0300

    Alteração do nome da classe e chamdas para que fique em conformidade da responsabilidade da classe, ConsultaUsuario.

--- controller/Login.php
+++ controller/Login.php
@@ -1,33 +1,51 @@
 <?php
-include("C:\wamp64\www\studio\controller\usuario\VerificaLogin.php");
+include("C:\wamp64\www\studio\controller\usuario\VerificaUsuario.php");
 include("C:\wamp64\www\studio\controller\DadosLogin.php");
+//include("C:\wamp64\www\studio\model\dadossessao.php");
 
 $dadosLogin = new DadosLogin;
-$VerificaLogin = new verificaLogin;
+$ConsultaUsuario = new ConsultaUsuario;
+//$dadossessao = new dadossessao;
 
-function login($VerificaLogin, $dadosLogin)
+function login($ConsultaUsuario, $dadosLogin)
 {
-    $usuario = $VerificaLogin->verificaLogin($dadosLogin->dadosLogin());
+    $usuario = $ConsultaUsuario->consultaUsuario($dadosLogin->dadosLogin());
     if (!empty($usuario)){
-       // -session_start();
+        //fechaSessao();
+        session_start();
+        session_unset();
         sessao($usuario);
+        
+        //var_dump($_SESSION);
         header("Location: \studio\sitemanutecao.php");
-        echo "<br>Parabens " . $usuario['nome']. " esta logado e seu id é: " . $usuario['id'];
+        //echo "<br>Parabens " . $usuario['nome']. " esta logado e seu id é: " . $usuario['id'];
     } else {
         header("Location: \studio\Errologin.php");
     }
 }
 
-function sessao($dados)
+function fechaSessao()
 {
-    session_start();
-    $_SESSION['email'] = $dados['email'];
-    $_SESSION['password'] = $dados['senha'];
+    if (session_status() == PHP_SESSION_ACTIVE || session_status() == true)
+    {
+        session_unset();
+        session_destroy();
+    }
+    
+}
 
+function sessao($dados_banco)
+{
+    //id, nome, email, senha, data_cadastro, status (Campos do banco);
+    
+    $_SESSION['id'] = $dados_banco["id"];
+    $_SESSION['nome'] = $dados_banco["nome"];
+    $_SESSION['status'] = $dados_banco["status"];
+   
     return $_SESSION;
 }
 
 login($VerificaLogin, $dadosLogin);
 
-var_dump($dadosLogin->dadosLogin());
+//var_dump(sessao($dados['email']));
 ?>
