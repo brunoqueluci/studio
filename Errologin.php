70def213f34b3b6dc183df5e40bda24e7b7eab9a
Bruno Queluci <brunoqueluci@gmail.com>
2022-10-20 11:22:02 -0300

    adicionado botão para retorno ao index.

--- Errologin.php
+++ Errologin.php
@@ -15,6 +15,11 @@
         <hr>
         <p>Verifique com adminstrador do sistema sobre esta mensagem.</p>
         <p>Dados Erro: e-mail ou senha inválidos.</p>
+        <div class="login-button">
+            <a href="\studio\controller\Logout.php">
+                <button type="button" name="Logout" value="Logout">SAIR</button>
+            </a>
+        </div>
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
