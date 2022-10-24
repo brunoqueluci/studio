752f34c44c242b1f2bef8fd2c0ce59717eef7fb4
Bruno Queluci <brunoqueluci@gmail.com>
2022-10-23 21:16:16 -0300

    Alterado o nome e link da página de cadastro de produto.

--- home.php
+++ home.php
@@ -66,7 +66,7 @@
             </a>
           </li>
           <li>
-            <a href="View\Usuario\cadastro.php" class="nav-link text-white">
+            <a href="cadastroproduto.php" class="nav-link text-white">
               <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#grid"/></svg>
               Cadastro
             </a>
