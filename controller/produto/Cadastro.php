794b4676df2c2be301e6db9ee6e27472616cc971
Bruno Queluci <brunoqueluci@gmail.com>
2022-10-23 22:40:43 -0300

    primeira modificação do classe que executa o cadastro.

--- controller/produto/Cadastro.php
+++ controller/produto/Cadastro.php
@@ -1,7 +1,17 @@
 <?php
-include("studio\controller\produto\GetDados.php");
+include("C:\wamp64\www\studio\controller\produto\GetDados.php");
+include("C:\wamp64\www\studio\model\produto\CadastraProduto.php");
 
 $GetDadosProduto = new GetDadosProduto;
-$GetDadosProduto->getDadosProduto();
+//$GetDadosProduto->getDadosProduto();
 
-var_dump($GetDadosProduto);
+$CadastraProduto = new CadastraProduto;
+//$CadastraProduto->cadastraProduto($GetDadosProduto);
+
+function cadastraProd($GetDadosProduto, $CadastraProduto)
+{
+    $CadastraProduto->cadastraProduto($GetDadosProduto);
+}
+
+var_dump($CadastraProduto->cadastraProduto($query));
+//var_dump($GetDadosProduto->getDadosProduto());
