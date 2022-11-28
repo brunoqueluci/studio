79dcf3ad2beedbd067aa7b742f22d3f8fa00864c
Bruno Queluci <brunoqueluci@gmail.com>
2022-11-28 11:10:56 -0300

    Arquivo ajax para cadastro do produto.

--- js/produto/cadastroProduto.js
+++ js/produto/cadastroProduto.js
@@ -1,31 +1,38 @@
+
 $('#cadastroproduto').submit(function(e){
     e.preventDefault();
 
-    var codigo = $('#codigo').val();
-    var descricao_produto = $('#descricao_produto').val();
-    var setor = $('#setor').val();
-    var valor_venda = $('#valor_venda').val();
-    var valor_custo = $('#valor_custo').val();
-    var estoque = $('#estoque').val();
-    var obs_produto = $('#obs_produto').val();
+    var dadosproduto = [];
+        dadosproduto["codigo"] = $('#codigo').val(),
+        dadosproduto["descricao_produto"] = $('#descricao_produto').val(),
+        dadosproduto["setor"] = $('#setor').val(),
+        dadosproduto["valor_venda"] = $('#valor_venda').val(),
+        dadosproduto["valor_custo"] = $('#valor_custo').val(),
+        dadosproduto["estoque"] = $('#estoque').val(),
+        dadosproduto["obs_produto"] = $('#obs_produto').val()
 
-    $.ajax({
-        url: 'controller/produto/Cadastro.php',
-        method: 'POST',
-        data: {
-            codigo: codigo,
-            descricao_produto: descricao_produto,
-            setor: setor,
-            valor_venda: valor_venda,
-            valor_custo: valor_custo,
-            estoque: estoque,
-            obs_produto: obs_produto
-        },
-        dataType: 'json'
-    }).done(function(result){
-        console.log(result);
-    });
+        console.log(dadosproduto);
+
+    // $.ajax({
+    //     url: 'controller/produto/Cadastro.php',
+    //     method: 'POST',
+    //     data: {
+    //         codigo: codigo,
+    //         descricao_produto: descricao_produto,
+    //         setor: setor,
+    //         valor_venda: valor_venda,
+    //         valor_custo: valor_custo,
+    //         estoque: estoque,
+    //         obs_produto: obs_produto
+    //     },
+    //     dataType: 'json',
+    //     sucess: function(data) {
+    //         console.log(data);
+    //     },
+    //     error: console.log("error")
+
+    // // }).done(function(result){
+    // //     console.log(result);
+    // });
 });
-
-
 
